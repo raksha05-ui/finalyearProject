abd876c9fa433cdca2f0792cc96eb2aa1ede3041
--get <--get>
2026-09-02 11:30:44 +0530

    added

--- inspect_bytes.py
+++ inspect_bytes.py
@@ -1,13 +1,8 @@
-p='main.py'
+p='main.py'
+with open(p,'rb') as f:
+    b=f.read()
+print('len=',len(b))
+print(b[:400])
+print(b[-200:])
 
-with open(p,'rb') as f:
-
-    b=f.read()
-
-print('len=',len(b))
-
-print(b[:400])
-
-
-print(b[-200:])
 
