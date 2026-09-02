e87b1d4ad9e5bd1774a8683ee296b7d3d8081992
--get <--get>
2026-09-02 11:29:33 +0530

    added

--- parse_main_syntax.py
+++ parse_main_syntax.py
@@ -1,32 +1,14 @@
 import ast
-
-
 import sys
 p='main.py'
 try:
-
-
     s=open(p,'r',encoding='utf-8').read()
-
-
     ast.parse(s)
-
-
     print('OK')
-
-
 except SyntaxError as e:
-
-
     print('SyntaxError', e.lineno, e.offset)
-
-
     if e.text:
-
-
         print('LINE:', e.text.rstrip())
-
-
     sys.exit(1)
 
 
